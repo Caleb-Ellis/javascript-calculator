4a1ce705c47163004b70a6c7f3ca0b646648e337
John Wright <50166727+johnwright-mozaic@users.noreply.github.com>
2021-11-22 11:02:13 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,7 +27,8 @@ $(document).ready(function() {
       case "*":
       case "/":
 	// fix this
-	eqn -= log + entry;
+	// eqn += log + entry;
+	eqn += log + 52;	    
         log = "0";
         break;
       case "=":
